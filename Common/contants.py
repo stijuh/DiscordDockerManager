e8a8588431101d361df5d622fb4f4c3fbfd9273f
Stijuh <stijevoh@gmail.com>
2023-08-24 20:32:46 +0200

    test yes

--- Common/contants.py
+++ Common/contants.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 APP_VERSION = "0.8.6"
 BOT_MENTION_ID = "<@1127985097564495974>"
 APP_NAME = "docker-manager-discord"
--- Entities/DockerManagerClient.py
+++ Entities/DockerManagerClient.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import discord
 from discord import app_commands
 
